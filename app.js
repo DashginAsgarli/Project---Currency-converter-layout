f8805b4a40d8644425fcb9c30f835cd362d7fc7c
DashginAsgarli <dashqinasgarli@gmail.com>
2025-12-15 02:03:33 +0400

    update

--- app.js
+++ app.js
@@ -8,81 +8,76 @@ let leftBox = document.querySelector(".left-box");
 let rightBox = document.querySelector(".right-box");
 let allİnput = document.querySelectorAll("input");
 let wifi = document.querySelector(".internet");
-
 wifi.style.display = "none"
 
 //Inputa yazilanlar duzgunluyun yoxlayiram nöqtə və reqemlerden başqa heçnə daxil etmək olmur  
-allİnput.forEach(a => a.addEventListener("keyup", e => e.target.value = e.target.value.replace(/,/g, ".").replace(/(\..*)\.|[^0-9.]/g, "$1")));
-
+allİnput.forEach(a =>
+    a.addEventListener("input", e => {
+        let val = e.target.value.replace(/,/g, ".").replace(/[^0-9.]/g, "");
+        if (val.indexOf(".") !== -1) {
+            let parts = val.split(".");
+            parts[1] = parts[1].slice(0, 5);
+            val = parts[0] + "." + parts[1];
+        } e.target.value = val;
+    }));
 // İnterneti yoxlamaq (chatdan kömək almişam)
-function checkWifi() {
-    wifi.style.display = navigator.onLine ? "none" : "block";
-    return navigator.onLine;
-}
-window.addEventListener("online", () => { wifi.style.display = "none"; convert() });
+window.addEventListener("online", () => { wifi.style.display = "none"; change() });
 window.addEventListener("offline", () => wifi.style.display = "block");
 
 // esas hesablama funksiyasi
+leftInput.value = 1;
 let lr = "left";
-function convert() {
+function change() {
     if (lr === "left") {
         input = leftInput;
         output = rightInput;
         from = activeLeft.innerHTML;
         to = activeRight.innerHTML;
-        amount = parseFloat(input.value);
+        amount = input.value;
+
     } else {
         input = rightInput;
         output = leftInput;
         from = activeRight.innerHTML;
         to = activeLeft.innerHTML;
-        amount = parseFloat(input.value);
+        amount = input.value;
     }
 
-    if (input.value == "") {
-        output.value = "";
-    }
+    if (input.value == "") { output.value = "" }
     else if (from === to) {
         output.value = amount;
         leftBox.innerHTML = `1 ${from} = 1 ${to}`;
         rightBox.innerHTML = `1 ${to} = 1 ${from}`;
     }
     else {
-        fetch(`https://api.exchangerate.host/convert?access_key=98130d664403c7c6a6a890d5c70d6d6a&from=${from}&to=${to}&amount=${amount}`)
+        fetch(`https://api.exchangerate.host/convert?access_key=9edfbed82a74b6fc2d52f2509a0b465f&from=${from}&to=${to}&amount=${amount}`)
             .then(res => res.json())
             .then(data => {
-                output.value = data.result.toFixed(4);
-
-                let rate = data.result / amount;
-                leftBox.innerHTML = `1 ${from} = ${rate.toFixed(5)} ${to}`;
-                rightBox.innerHTML = `1 ${to} = ${(1 / rate).toFixed(5)} ${from}`;
+                output.value = data.result.toFixed(5);
+                leftBox.innerHTML = `1 ${from} = ${data.info.quote.toFixed(5)} ${to}`;
+                rightBox.innerHTML = `1 ${to} = ${(1 / data.info.quote).toFixed(5)} ${from}`;
             })
     }
 }
+change();
 
 // inputlarin daxil edilen reqemlere esasen cevirme edir 
-leftInput.addEventListener("keyup", () => { lr = "left"; convert(); });
-rightInput.addEventListener("keyup", () => { lr = "right"; convert(); });
-
-// SOL VALYUTA SEÇİMİ
+leftInput.addEventListener("input", () => { lr = "left"; change(); });
+rightInput.addEventListener("input", () => { lr = "right"; change(); });
+// klike esasen
 pLeft.forEach(p => {
     p.addEventListener("click", () => {
         activeLeft.classList.remove("color");
         p.classList.add("color");
         activeLeft = p;
-        convert();
+        change();
     });
 });
-
-// SAĞ VALYUTA SEÇİMİ
 pRight.forEach(p => {
     p.addEventListener("click", () => {
         activeRight.classList.remove("color");
         p.classList.add("color");
         activeRight = p;
-        convert();
+        change();
     });
 });
-// qisalt
-convert();
-// // 130
